ca37d679c402dfc0fcb7a40e516f56e041b03246
Guy Carver <gdcarver@yahoo.com>
2021-08-25 20:38:21 -0400

    adding dte directory

--- dte/__init__.py
+++ dte/__init__.py
@@ -4,6 +4,7 @@ from ctypes import CDLL, POINTER, byref, c_bool, c_uint, c_void_p, c_wchar_p
 _lib = CDLL(__path__[0] + '/VisualStudio.dll')
 _lib.GetBreakPoints.restype = POINTER(c_void_p)
 _lib.GetBreakPoint.restype = c_wchar_p
+_lib.GetEnabledBreakPoint.restype = c_wchar_p
 
 #--------------------------------------------------------
 def open( aVersion ):
@@ -11,7 +12,8 @@ def open( aVersion ):
 
 #--------------------------------------------------------
 def setfile( aPath, aLine ):
-  _lib.SetFile(aPath, aLine)
+  b = _lib.SetFile(aPath, aLine)
+  return b
 
 #--------------------------------------------------------
 def breakpoints(  ):
